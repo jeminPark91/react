aacf43a3bc5f2790bb6b9623f5c10a271685281a
= <wpals4455@naver.com>
2022-08-08 21:09:15 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
+import AddTodo from "./AddTodo.js";
+import {Paper, List, Container} from "@material-ui/core";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      //item : {id : 0, title : "Hello world 1", done : true}
+      items : [
+        { id : "0", title : "Hello world 1", done: true},
+        { id : "1", title : "Hello world 2", done: false},
+      ],
+    };
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items : thisItems});
+    console.log("items : ", this.state.items);
+  }
+  
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update ITems : ", this.state.items);
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items:newItems},() =>{
+      console.log("Update Items : ", this.state.items);
+    });
+  }
+
+  render(){
+
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin : 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete}/>
+          ))}
+        </List>
+      </Paper>
+    );
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    ListItem, 
+    ListItemText, 
+    InputBase, 
+    Checkbox,
+    ListItemSecondaryAction,
+    IconButton
+} from "@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+
+class Todo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {item : props.item};
+        this.delete = props.delete;
+    }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
+    }
+
+    render(){
+        const item = this.state.item;
+
+        return (
+            <ListItem>
+                <Checkbox checked={item.done}/>
+                <ListItemText>
+                    <InputBase
+                        inputProps={{ "aria-label" : "naked"}}
+                        type="text"
+                        id={item.id}
+                        name={item.name}
+                        value={item.title}
+                        multiline={true}
+                        fullWidth={true}
+                    />
+                </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete Todo"
+                    onClick={this.deleteEventHandler}>
+                        <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+            
+
+            // <div classNAme = "Todo">
+            //     <input type="checkbox" 
+            //     id={this.state.item.id} 
+            //     name={this.state.item.name} 
+            //     checked={this.state.item.done} />
+            //     <label id={this.state.item.id}>{this.state.item.title}</label>
+            // </div>
+        );
+    }
+}
+export default Todo;
